41c67480d185da25ea130625835f5227c1e8cb4b
Sergiolm17 <kingsergiolm17@gmail.com>
2022-06-21 20:36:25 -0500

    interface

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -6,12 +6,26 @@ interface sendMessage {
     data?: any;
     gifPlayback?: boolean;
 }
+export interface Commet {
+    name: string;
+    time: number;
+}
+export interface RootObject {
+    status: string;
+    total: number;
+    validPhoneNumbers: number;
+    numbersFilter: number;
+    invalidPhoneNumbers: string[];
+    campaingid: string;
+    commets: Commet[];
+    instanceid: string;
+}
 export declare class Client {
     private token;
     constructor(token: string);
-    text(data: sendMessage): Promise<any>;
-    sticker(data: sendMessage): Promise<any>;
-    image(data: sendMessage): Promise<any>;
-    video(data: sendMessage): Promise<any>;
+    text(data: sendMessage): Promise<RootObject>;
+    sticker(data: sendMessage): Promise<RootObject>;
+    image(data: sendMessage): Promise<RootObject>;
+    video(data: sendMessage): Promise<RootObject>;
 }
 export {};
